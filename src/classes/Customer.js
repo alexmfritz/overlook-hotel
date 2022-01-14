217b303ac8c8a38e1da82206311e285cfbc5058e
Alex Fritz <alexfritzcoach@gmail.comm>
2022-01-14 14:29:04 -0800

    refactor customer class to not need to take in all booking and room information

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,26 +1,26 @@
 export default class Customer {
-  constructor(customerObj, allBookingsData, allRoomsData) {
+  constructor(customerObj) {
     this.id = customerObj.id;
     this.name = customerObj.name;
-    this.bookings = this.getCustomerBookings(allBookingsData) || [];
-    this.totalSpent = this.getTotalCustomerAmountSpent(allRoomsData);
+    this.bookings =  [];
+    this.totalSpent = 0;
   }
 
   getCustomerBookings(bookings) {
-    return bookings.filter(booking => booking.userID === this.id);
+    this.bookings = bookings.filter(booking => booking.userID === this.id);
   }
 
   getTotalCustomerAmountSpent(rooms) {
     if (this.bookings.length === 0) {
       return 0;
     }
-    return this.bookings.reduce((totalPrice, booking) => {
+    this.totalSpent = this.bookings.reduce((totalPrice, booking) => {
       rooms.forEach(room => {
         if (booking.roomNumber === room.number) {
           totalPrice += room.costPerNight;
         }
       })
       return totalPrice;
-    }, 0)
+    }, 0);
   }
 }
