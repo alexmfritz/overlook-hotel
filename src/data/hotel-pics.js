05f85f4c9bd54f506667ecea5bdfb00e231b907e
Alex Fritz <alexfritzcoach@gmail.comm>
2022-01-17 14:43:37 -0800

    Create hotelPics object in order to dynamically get hotel pics onto page based on roomType

---
+++ src/data/hotel-pics.js
@@ -0,0 +1,8 @@
+const hotelPics =  [
+  { roomType: 'residential suite', src: './images/residential-suite.png'},
+  { roomType: 'junior suite', src: './images/junior-suite.png'},
+  { roomType: 'suite', src: './images/suite.png'},
+  { roomType: 'single room', src: './images/single-room.png'}
+];
+
+export { hotelPics };
